76a105fc0820eb2e62b27d9913f10985a0a2d35c
undarmin <157483848+undarmin@users.noreply.github.com>
2024-02-17 13:57:57 +0530

    Increment Game.cheese on click through Game.click()

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 const cheese = document.querySelector("#cheese");
+const cheeseCount = document.querySelector("#cheese-count");
+const cpsCount = document.querySelector("#cps-count")
+
+// Game object
+
+Game = {
+    cheese: 0,
+    cps: 0,
+    clickPower: 1,
+    update() {
+        cheeseCount.textContent = Math.round(this.cheese);
+        cpsCount.textContent = Math.round(this.cps);
+    },
+    click() {
+        this.earn(this.clickPower);
+        this.update();
+    },
+    earn(cheese) {
+        this.cheese += cheese;
+    }
+};
 
 cheese.addEventListener('click', () => {
     cheese.classList.add("clicked");
-    setTimeout(() => {
+    wobbleTimeout = setTimeout(() => {
         cheese.classList.remove("clicked")
-    }, 500);
+    }, 50);
+    Game.click();
 })
